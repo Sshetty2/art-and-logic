90790db8252e557259f2e057d734068636dd6c59
Sachit <sachit.shetty@gmail.com>
2019-04-08 20:00:29 -0400

    delete extra content

--- src/App.jsx
+++ src/App.jsx
@@ -3,48 +3,53 @@ import logo from './logo.svg';
 import './App.css';
 import InternalTextField from './InternalTextField'
 
-
-
-const styles = {
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-}
-
-
-
-
-function toCelsius(fahrenheit) {
-  return (fahrenheit - 32) * 5 / 9;
-}
-
-function toFahrenheit(celsius) {
-  return (celsius * 9 / 5) + 32;
-}
-
-function tryConvert(temperature) {
-  const input = parseFloat(temperature);
-  if (Number.isNaN(input)) {
-    return '';
+function tryEncode(input) {
+  input = parseInt(input)
+  if(input < -8192 || input > 8191 ){
+    return ''
   }
-  const rounded = Math.round(input * 1000) / 1000;
-  return rounded.toString();
+  input = input + 8192;
+  let mask = 127;
+  let bit1 = input & mask;
+  let bit2 = (input >> 7) << 8;
+  let result = ( bit2 | bit1 ).toString(16);
+  return result
 }
 
-function BoilingVerdict(props) {
-  if (props.celsius >= 100) {
-    return <p>The water would boil.</p>;
+function tryDecode(val){
+  let input = val.toString()
+  let result = '';
+  if (input.length === 4){
+    let lowInt = input.slice(2,4)
+    let hiInt = input.slice(0,2)
+    let lowBit = parseInt(lowInt, 16)
+    let hiBit = parseInt(hiInt, 16)
+    if ( hiBit < parseInt("00", 16)  || 
+      hiBit > parseInt("7f", 16)  ||
+      lowBit < parseInt("00", 16) ||
+      lowBit > parseInt("7f", 16) ){
+      return result;
+    } else {
+        hiBit = hiBit << 7;
+        result =  ( hiBit | lowBit ) - 8192;
+        result = result.toString(10);    
+    }
   }
-  return <p>The water would not boil.</p>;
+  return result
 }
 
+function DescriptionText(props) {
+  if (props.encoded.length >= 3 && props.decoded.length >= 4) {
+    return <p>Voilà!</p>;
+  }
+  return <p>Enter in a value to Encode or Decode</p>;
+}
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.handleEncodedChange = this.handleEncodedChange.bind(this);
-    this.state = {temperature: '', scale: 'c'};
+    this.state = {_value: ""};
   }
 
   handleEncodedChange(_value){
@@ -63,8 +68,8 @@ class App extends Component {
 
     const varType = this.state.varType;
     const _value = this.state._value;
-    const encoded = varType === 'd' ? tryConvert(_value) : _value;
-    const decoded = varType === 'e' ? tryConvert(_value) : _value;
+    const encoded = varType === 'd' ? tryDecode(_value) : _value;
+    const decoded = varType === 'e' ? tryEncode(_value) : _value;
 
     return (
       <div className="App">
@@ -80,6 +85,7 @@ class App extends Component {
            _value={decoded}
            onInputChange={this.handleDecodedChange}
             />
+          <DescriptionText encoded={encoded} decoded={decoded} />
         </header>
       </div>
     );
